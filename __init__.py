663a536c0d2dc7fecdff1cc7ee6ffd0e2c691d35
CHMIELEWSKI Sebastian <sebastian.chmielewski@idemia.com>
2023-11-22 17:10:19 +0100

    create full dictionaries into disk file

--- __init__.py
+++ __init__.py
@@ -5,10 +5,14 @@ from binaryninja import *
 def create(bv):
     consts = set()
     for function in bv.functions:
+        if function.medium_level_il is None:
+            print(f"[-] No Medium Level IL for function at adddress {hex(function.start)}")
+            continue
         for const in get_consts_from_function(bv, function):
             consts.add(const)
-
-    print_dict_from(consts, bv.arch.address_size*4)
+    with open(f"{bv.file.filename}.dict", "w") as f:
+        print_dict_from(consts, bv.arch.address_size*4, f)
+        print(f"[+] file {bv.file.filename}.dict written")
 
 def get_consts_from_function(bv, func):
     consts = set()
@@ -27,20 +31,18 @@ def get_consts_from_function(bv, func):
 
 def create_for_function(bv, func):
     consts = get_consts_from_function(bv, func)
-    print_dict_from(consts, bv.arch.address_size*4)
+    print_dict_from(consts, bv.arch.address_size*4,sys.stdout)
 
-def print_dict_from(consts, bitsize=32):
-    print("#begin dictionary:"+"="*40)
+def print_dict_from(consts, bitsize=32,fd=sys.stdout):
     for idx, const in enumerate(consts):
         if bitsize == 16:
-            print("kw{}=\"\\x{:>04x}\"".format(idx, const))
+            print("kw{}=\"\\x{:>04x}\"".format(idx, const),file=fd)
         elif bitsize == 32:
-            print("kw{}=\"\\x{:>08x}\"".format(idx, const))
+            print("kw{}=\"\\x{:>08x}\"".format(idx, const),file=fd)
         elif bitsize == 64:
-            print("kw{}=\"\\x{:>016x}\"".format(idx, const))
+            print("kw{}=\"\\x{:>016x}\"".format(idx, const),file=fd)
         else:
             raise Exception("Unsupported bitsize: %d" % bitsize)
-    print("#end dictionary"+"="*43)
 
-PluginCommand.register("Create dictionary", "Attempt to create a fuzzing dictionary out of constants in the program", create)
-PluginCommand.register_for_function("Create dictionary for function", "Attempt to create a fuzzing dictionary out of constants for this function", create_for_function)
+PluginCommand.register("Dictionary\\Create dictionary", "Attempt to create a fuzzing dictionary out of constants in the program", create)
+PluginCommand.register_for_function("Dictionary\\Create dictionary for function", "Attempt to create a fuzzing dictionary out of constants for this function", create_for_function)
